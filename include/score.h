eebdc6c6bd45fbb14a5f10b96ef040224771ae35
flashbomb625 <a01080065205@gmail.com>
2025-06-01 01:40:38 +0900

    Update score.h

--- include/score.h
+++ include/score.h
@@ -1,2 +1,14 @@
+#ifndef SCORE_H
+#define SCORE_H
 
+#include "physics.h"
+#include <stdlib.h>
 
+extern int score;
+//뱀이 사과를 먹었는지 1->먹음 0->안먹음
+int Snake_eat_apple(position snakeHead, Size apple);
+
+//먹은 사과의 크기에 따라 점수 증가. 크기 1당 10
+void increase_score();
+
+#endif

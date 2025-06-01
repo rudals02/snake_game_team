f1127ad3f45d63f10f9502a6b270695918adfb52
codejwj <jsin1336@gmail.com>
2025-06-01 20:56:31 +0900

    Update snake.h

--- include/snake.h
+++ include/snake.h
@@ -1,2 +1,21 @@
+// snake.h
+#ifndef SNAKE_H
+#define SNAKE_H
 
+#include "physics.h"
 
+typedef enum { UP, DOWN, LEFT, RIGHT } direction;
+
+extern position snake[];
+extern int snake_length;
+extern int gameover;
+extern Size apple;
+
+void init_snake(void);
+void move_snake(void);
+void grow_snake(int size);
+void draw_snake(void);
+void draw_apple(void);
+void set_direction(direction dir);
+
+#endif

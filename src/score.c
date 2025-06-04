486f934a1120c5c94194b4776b7d4fababa8b007
flashbomb625 <a01080065205@gmail.com>
2025-06-04 15:34:59 +0900

    Update score.c

--- src/score.c
+++ src/score.c
@@ -4,7 +4,7 @@
 int score = 0;
 
 
-//뱀이 사과와 닿으면 1
+//뱀이 사과와 닿으면 점수 추가하기
 int Snake_eat_apple(position snakeHead, Size apple) 
 {
      return (snakeHead.x >= apple.pos.x - apple.size && snakeHead.x <= apple.pos.x + apple.size &&
@@ -15,7 +15,7 @@ extern position snake[];
 extern Size apple;
 
 
-//스네이크의 머리가 사과와 닿았을 경우
+//스네이크의 머리가 사과와 닿았는지 확?인 하기
 void increase_score() 
 {
      if (Snake_eat_apple(snake[0], apple))
